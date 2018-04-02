c9aa811620790d1844f29b99ddc5ad669abcbdd2
Abhishek Singhal <abhi.singhal95@gmail.com>
2018-04-02 04:52:32 -0400

    mapbox update, sample loc file added
    -differenet mapbox deployed
    -sample loc file added
    - real time update happening now!!
    - client server interation complete

--- src/main.py
+++ src/main.py
@@ -1,46 +1,4 @@
-from flask import Flask, render_template, jsonify
-import time
-import pickle
-from flask.ext.socketio import SocketIO, emit
+f = open('sample_loc.txt','r')
 
-fp = open("shared.pkl",'rb')
-
-
-values = {
-    'lat': 23,
-    'lon': -72,
-}
-
-#################################################################################
-
-
-###################################################################################
-
-app = Flask(__name__)
-socketio = SocketIO(app)
-
-
-@app.route("/loc/")
-def index():
-    #return jsonify(lat=lat,lon=lon)
-    return render_template("webpage.html",**values)
-
-@socketio.on('value changed')
-def value_changed(message):
-    try:
-        shared = pickle.load(fp)
-        values['lat'] = shared['lat']
-        values['lon'] = shared['lon']
-        print (values)
-    except:
-        print ('no update')
-    emit('update value', message, broadcast=True)
-    
-    
-
-if __name__=="__main__":
-        socketio.run(app,host='127.0.0.1')
-        
-        
-        
-        
+line = f.readline()[:-1].split(",")
+line = f.readline()[:-1].split(",")
--- src/sockettestserver.py
+++ src/sockettestserver.py
@@ -4,10 +4,14 @@ from flask_socketio import SocketIO, emit,send
 import pickle
 import logging
 
+
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-fp = open("shared.pkl",'rb')
+#fp = open("shared.pkl",'rb')
+fp = open("sample_loc.txt",'r')
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
@@ -30,15 +34,18 @@ def handle_client_connect_event(json):
 @socketio.on('value changed')
 def value_changed(message):
     try:
-        shared = pickle.load(fp)
-        values['lat'] = shared['lat']
-        values['lon'] = shared['lon']
+        #shared = pickle.load(fp)
+        #values['lat'] = shared['lat']
+        #values['lon'] = shared['lon']
+        line = fp.readline()[:-1].split(",")
+        values['lat'] =line[0]
+        values['lon'] =line[1]
         print (values)
-        update_message = values
+        
     except:
         print ('no update')
-        update_message = 'no update'
-    
+        
+    update_message = values
     emit('update value', update_message, broadcast=True)
         
 
